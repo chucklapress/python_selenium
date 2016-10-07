d6519db2f52bec07fa4fc0f2c613f5dfd851efb4
chucklapress <charleslapress@gmail.com>
2016-10-07 16:12:01 -0400

    automating with selenium

---
+++ test1.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+
+browser.get('http://www.yahoo.com')
+assert 'Yahoo' in browser.title
+
+elem = browser.find_element_by_name('p')  # Find the search box
+elem.send_keys('Santa Claus Conquers the Martians' + Keys.RETURN)
