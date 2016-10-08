65c49db5c66978f6208e6665ba8d5cb4771f383e
chucklapress <charleslapress@gmail.com>
2016-10-07 20:04:57 -0400

    modifing test adding browser exit

--- test1.py
+++ test1.py
@@ -6,5 +6,7 @@ browser = webdriver.Chrome()
 browser.get('http://www.yahoo.com')
 assert 'Yahoo' in browser.title
 
-elem = browser.find_element_by_name('p')  # Find the search box
+elem = browser.find_element_by_name('p')
 elem.send_keys('Santa Claus Conquers the Martians' + Keys.RETURN)
+
+browser.quit()
