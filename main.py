60c57d2a0df7f23768056c30128dfa402903e783
chucklapress <charleslapress@gmail.com>
2017-01-13 15:17:33 -0500

    removrd unneeded imports

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
@@ -12,6 +9,6 @@ assert 'Google' in browser.title
 search = browser.find_element_by_name('q')
 search.send_keys('smokingpipes.com Alex Florov Sandblasted Ryukin')
 search.send_keys(Keys.RETURN)
-time.sleep(10)
+time.sleep(5)
 
 browser.quit()
